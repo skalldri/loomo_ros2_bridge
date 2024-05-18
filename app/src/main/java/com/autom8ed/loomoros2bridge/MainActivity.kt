519119697c7900e0ab2eacfc74872bbc47c36f7e
Stuart Alldritt <s.k.alldritt@gmail.com>
2024-05-18 12:35:01 -0700

    Initial Commit

---
+++ app/src/main/java/com/autom8ed/loomoros2bridge/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.autom8ed.loomoros2bridge
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.autom8ed.loomoros2bridge.ui.theme.LoomoROS2BridgeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LoomoROS2BridgeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LoomoROS2BridgeTheme {
+        Greeting("Android")
+    }
+}
